b3cbd13f79f42f3d07efd07d88e28d6d771f726f
king-of-Jupiter <125120167+king-of-Jupiter@users.noreply.github.com>
2024-03-25 01:43:36 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,18 @@ apt update && apt upgrade -y
 apt-get install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils google-chrome-stable -y
 apt install xrdp -y && systemctl enable xrdp
 adduser xrdp ssl-cert
+cat << E0F > /etc/polkit-1/localauthority/50-local.d/45-allow.colord.pkla
 
+[Allow Colord all Users]
+
+Identity=unix-user:*
+
+Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+
+ResultAny=no
+
+ResultInactive=no
+
+ResultActive=yes
+
+E0F
