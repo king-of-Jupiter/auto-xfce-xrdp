903d410cba7530ecda0a28ceac8586a5e3db4d1a
king-of-Jupiter <125120167+king-of-Jupiter@users.noreply.github.com>
2024-03-27 01:30:50 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,18 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt update && apt upgrade -y
 sudo apt-get install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils google-chrome-stable -y
-sudo apt install xrdp -y && systemctl enable xrdp
+sudo apt install xrdp xorgxrdp -y && sudo systemctl enable xrdp
 sudo adduser xrdp ssl-cert
+cd /etc/xrdp
+sudo mv startwm.sh startwm.sh.bak
+sudo echo '#!/bin/sh' > /etc/xrdp/startwm.sh
+sudo echo 'if [ -r /etc/default/locale ]; then' >> /etc/xrdp/startwm.sh
+sudo echo '. /etc/default/locale' >> /etc/xrdp/startwm.sh
+sudo echo 'export LANG LANGUAGE' >> /etc/xrdp/startwm.sh
+sudo echo 'fi' >> /etc/xrdp/startwm.sh
+sudo echo 'exec /usr/bin/startxfce4' >> /etc/xrdp/startwm.sh
+sudo chmod 755 startwm.sh
+sudo systemctl restart xrdp
 sudo cat << E0F > /etc/polkit-1/localauthority/50-local.d/45-allow.colord.pkla
 
 [Allow Colord all Users]
@@ -25,20 +35,3 @@ ResultActive=yes
 
 E0F
 
-desktop_file="/home/sammy/Desktop/google-chrome.desktop"
-
-cat <<EOT > $desktop_file
-[Desktop Entry]
-Version=1.0
-Name=Google Chrome
-Exec=/usr/bin/google-chrome-stable %U
-StartupNotify=true
-Terminal=false
-Icon=google-chrome
-Type=Application
-Categories=Network;WebBrowser;
-MimeType=application/pdf;application/rdf+xml;application/rss+xml;application/xhtml+xml;application/xhtml_xml;application/xml;image/gif;image/jpeg;image/png;image/webp;text/html;text/xml;x-scheme-handler/http;x-scheme-handler/https;
-Actions=new-window;new-private-window;
-EOT
-
-chmod 755 $desktop_file
