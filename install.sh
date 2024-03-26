f28998c5b26f5fbe3ebf0ec9a85e6b9052dd0aec
king-of-Jupiter <125120167+king-of-Jupiter@users.noreply.github.com>
2024-03-27 00:45:44 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-apt -y remove needrestart
+sudo apt -y remove needrestart
 sudo useradd -m sammy
 echo "sammy:sammy" | sudo chpasswd
-usermod -aG sudo sammy
+sudo usermod -aG sudo sammy
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-apt update && apt upgrade -y
-apt-get install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils google-chrome-stable -y
-apt install xrdp -y && systemctl enable xrdp
-adduser xrdp ssl-cert
-cat << E0F > /etc/polkit-1/localauthority/50-local.d/45-allow.colord.pkla
+sudo apt update && apt upgrade -y
+sudo apt-get install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils google-chrome-stable -y
+sudo apt install xrdp -y && systemctl enable xrdp
+sudo adduser xrdp ssl-cert
+sudo cat << E0F > /etc/polkit-1/localauthority/50-local.d/45-allow.colord.pkla
 
 [Allow Colord all Users]
 
